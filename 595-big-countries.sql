128f8cd6988a155d1126345224edea04e87e1620
AllenliIE <allen.li.service@gmail.com>
2022-08-02 11:04:10 +0800

    feat: 595-big-countries.sql

---
+++ 595-big-countries.sql
@@ -0,0 +1,11 @@
+//Blog: http://52.198.119.162/2022/07/18/leetcode-mysql-595-big-countries/
+
+-- <strong>Solution:</strong>
+-- 1. 選擇標題「name」、「population」、「area」
+-- 2. 來自於 World 的 table
+-- 3. 設定條件「area >= 3000000」or「population >= 25000000」
+
+SELECT name, population, area 
+	FROM World
+Where area >= 3000000
+	OR population >= 25000000;
