bafefbeff047cd777d46ebf7c7c3e09d20db4958
AllenliIE <allen.li.service@gmail.com>
2022-08-04 11:32:57 +0800

    feat: create 627-swap-salary.sql

---
+++ 627-swap-salary.sql
@@ -0,0 +1,10 @@
+//Blog: http://52.198.119.162/2022/07/20/leetcode-mysql-627-swap-salary/
+
+-- <strong>Solution:</strong>
+-- 1. 更新 Salary 的 table
+-- 2. 設定「CASE WHEN」判定當 sex = 'f'，則改為'm'，不是'f'的改為'f'。
+-- 3. 顯示 Salary 的 table
+
+UPDATE Salary 
+SET sex = (CASE WHEN sex = 'f' then 'm' ELSE 'f' END);
+SELECT * FROM Salary;
