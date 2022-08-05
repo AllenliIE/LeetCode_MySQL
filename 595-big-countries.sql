c35acd7f3fa6fa321d9b9d6e11b4f1dd930a7f47
AllenliIE <allen.li.service@gmail.com>
2022-08-05 14:14:14 +0800

    feat: create 595-big-countries.sql

--- 595-big-countries.sql
+++ 595-big-countries.sql
@@ -5,6 +5,7 @@
 -- 2. 來自於 World 的 table
 -- 3. 設定條件「area >= 3000000」or「population >= 25000000」
 
+-- <strong>Code1:</strong>
 SELECT name, population, area 
 	FROM World
 Where area >= 3000000 OR population >= 25000000;
