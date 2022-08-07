2a58777fd5c7d53b4d72b71e7de5b007abcbc665
AllenliIE <allen.li.service@gmail.com>
2022-08-07 14:36:09 +0800

    feat: create 182-duplicate-emails.sql

---
+++ 182-duplicate-emails.sql
@@ -0,0 +1,13 @@
+//Blog: http://52.198.119.162/2022/08/06/leetcode-mysql-182-duplicate-emails/
+
+-- <strong>Solution:</strong>
+-- 1. 確認標題 email 為 Email 。
+-- 2. 來自 Person 的 table。
+-- 3. 以 email 為群組。
+-- 4. 有 Email 次數大於1。
+
+-- <strong>Code.1:</strong>
+SELECT email AS Email
+  FROM Person
+GROUP BY email
+HAVING COUNT(Email) > 1;
