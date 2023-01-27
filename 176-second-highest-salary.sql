540fc921a0d94fb3a0bfdd5e47b6afd629448fe8
AllenliIE <allen.li.service@gmail.com>
2023-01-27 23:55:45 +0800

    feat: modify 176-second-highest-salary.sql

--- 176-second-highest-salary.sql
+++ 176-second-highest-salary.sql
@@ -12,3 +12,8 @@ SELECT
         ORDER BY Salary DESC
         LIMIT 1 OFFSET 1
 	) AS SecondhighestSalary;
+
+-- <strong>Code2:</strong>
+SELECT MAX(salary) AS SecondHighestSalary
+    FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
