ccf6efcb483f8208babc6440538bd1d606999071
AllenliIE <allen.li.service@gmail.com>
2023-01-30 23:29:33 +0800

    feat: modify 181-employees-earning-more-than-their-managers.sql

--- 181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers.sql
@@ -5,9 +5,16 @@
 -- 2. 將 Employee 拆為 t1, t2 兩張表進行比較。
 -- 3. 以 t1.managerId 為基準，且符合 t2.id 中有 salary 比老闆高者。
 
--- <strong>Code.1:</strong>
+-- <strong>Code.1:</strong>  
 SELECT t1.name AS Employee
 	FROM Employee AS t1
 LEFT JOIN Employee AS t2
+	ON t1.managerId = t2.id
+WHERE t1.salary > t2.salary;
+
+-- <strong>Code.2:</strong>  
+SELECT t1.name AS Employee
+	FROM Employee AS t1
+INNER JOIN Employee AS t2
 	ON t1.managerId = t2.id
 WHERE t1.salary > t2.salary;
